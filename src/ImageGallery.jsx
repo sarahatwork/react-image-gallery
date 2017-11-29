0c2fd068cb85c78a71c42916628b8fbb43e10cf7
Marko Markovic <marko.markovic@work.co>
2017-11-29 11:54:01 +0100

    fix

--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -879,45 +879,36 @@ export default class ImageGallery extends React.Component {
 
     return (
       <div className='image-gallery-image'>
-        <img
-            src={item.original}
-            alt={item.originalAlt}
-            srcSet={item.srcSet}
-            sizes={item.sizes}
-            title={item.originalTitle}
-            onLoad={this.props.onImageLoad}
-            onError={onImageError}
-        />
         {
-          // item.srcSet ? (
-          //   <picture
-          //     onLoad={this.props.onImageLoad}
-          //     onError={onImageError}
-          //   >
-          //     {Object.keys(item.srcSet).map(breakpoint =>
-          //         item.srcSet[breakpoint].srcSet && (
-          //           <source
-          //             key={breakpoint}
-          //             media={item.media[breakpoint]}
-          //             srcSet={item.srcSet[breakpoint].srcSet}
-          //           />
-          //         ))}
-          //     <img
-          //       alt={item.originalAlt}
-          //       src={item.srcSet[Object.keys(item.srcSet)[Object.keys(item.srcSet).length - 1]].src}
-          //     />
-          //   </picture>
-          // ) : (
-          //   <img
-          //       src={item.original}
-          //       alt={item.originalAlt}
-          //       srcSet={item.srcSet}
-          //       sizes={item.sizes}
-          //       title={item.originalTitle}
-          //       onLoad={this.props.onImageLoad}
-          //       onError={onImageError}
-          //   />
-          // )
+          item.srcSet ? (
+            <picture
+              onLoad={this.props.onImageLoad}
+              onError={onImageError}
+            >
+              {Object.keys(item.srcSet).map(breakpoint =>
+                  item.srcSet[breakpoint].srcSet && (
+                    <source
+                      key={breakpoint}
+                      media={item.media[breakpoint]}
+                      srcSet={item.srcSet[breakpoint].srcSet}
+                    />
+                  ))}
+              <img
+                alt={item.originalAlt}
+                src={item.srcSet[Object.keys(item.srcSet)[Object.keys(item.srcSet).length - 1]].src}
+              />
+            </picture>
+          ) : (
+            <img
+                src={item.original}
+                alt={item.originalAlt}
+                srcSet={item.srcSet}
+                sizes={item.sizes}
+                title={item.originalTitle}
+                onLoad={this.props.onImageLoad}
+                onError={onImageError}
+            />
+          )
         }
 
         {
